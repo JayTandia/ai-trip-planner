28994c281a31fa0a2e4feab1f736fb03d39ac7df
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2024-07-29 22:11:11 +0530

    completed

---
+++ src/components/custom/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='flex justify-center items-center mb-5 '>
+    <div className='mt-2 '>Made by<strong className='underline'> <a target='_blank' href='https://github.com/JayTandia'>Jay Tandia</a></strong> with ❤️</div>
+    </div>
+  )
+}
+
+export default Footer
